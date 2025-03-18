e5304663860ff289ed769d0baf363ebf0e2bf683
USAMA ASHRAF <138494844+usamaashraf23@users.noreply.github.com>
2025-03-18 17:07:44 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import heroImg from "/src/assets/heroImg.png";
+import reactIcon from "/src/assets/backgroundImages/react.png";
+import globeIcon from "/src/assets/backgroundImages/globe.png";
+import vaccumIcon from "/src/assets/backgroundImages/vaccum.png";
+import triangleIcon from "/src/assets/backgroundImages/triangle.png";
+import scaleIcon from "/src/assets/backgroundImages/scale.png";
+import bulbsketchIcon from "/src/assets/backgroundImages/bulbsketch.png";
+import pcIcon from "/src/assets/backgroundImages/pc.png";
 
 function Hero() {
   return (
@@ -10,37 +17,37 @@ function Hero() {
     >
       {/* Background Images - Now Responsive */}
       <img
-        src="./src/assets/backgroundImages/react.png"
+        src={reactIcon}
         alt=""
         className="w-10 sm:w-16 absolute right-2 sm:right-8 top-10 sm:top-10"
       />
       <img
-        src="./src/assets/backgroundImages/globe.png"
+        src={globeIcon}
         alt=""
         className="w-10 sm:w-16 absolute right-1 sm:right-8 bottom-60 sm:bottom-20"
       />
       <img
-        src="./src/assets/backgroundImages/vaccum.png"
+        src={vaccumIcon}
         alt=""
         className="w-10 sm:w-16 absolute left-1 sm:left-20 bottom-60 sm:bottom-12"
       />
       <img
-        src="./src/assets/backgroundImages/triangle.png"
+        src={triangleIcon}
         alt=""
         className=" sm:block w-12 sm:w-20 absolute left-1 sm:left-80 bottom-24 sm:bottom-16"
       />
       <img
-        src="./src/assets/backgroundImages/scale.png"
+        src={scaleIcon}
         alt=""
         className="w-10 sm:w-14 absolute left-1 sm:left-96 top-1 sm:top-16"
       />
       <img
-        src="./src/assets/backgroundImages/bulbsketch.png"
+        src={bulbsketchIcon}
         alt=""
         className="hidden sm:block w-12 sm:w-16 absolute right-24 sm:right-64 top-12 sm:top-1"
       />
       <img
-        src="./src/assets/backgroundImages/pc.png"
+        src={pcIcon}
         alt=""
         className="hidden sm:block w-12 sm:w-16 absolute right-32 sm:right-96 top-12 sm:top-8"
       />
@@ -63,7 +70,7 @@ function Hero() {
           <input
             type="text"
             placeholder="Search for a location..."
-            className="flex-grow px-4 py-2 outline-none text-gray-700 w-full"
+            className="flex-grow px-4 py-2 text-sm outline-none text-gray-700 w-full"
           />
           <button className="bg-[#4D2C5E] text-white px-6 py-2 rounded-full hover:bg-purple-800 transition">
             Continue
